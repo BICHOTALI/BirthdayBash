b89be613f7ca48a2a4e3af5a8b1a49c1eea028c2
saradonnelly97 <sara.donnelly8@gmail.com>
2023-11-27 22:07:47 -0500

    added in models and config.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,24 @@
 //Dependencies
+const express = require('express')
+const methodOverride = require('method-override')
+require('dotenv').config()
+const userController = require('./controllers/users')
+const birthdayController = require('./controllers/birthdays')
 
+const app = express()
 const { Sequelize } = require('sequelize')
 require('dotenv').config();
 
 //Configuration/Middleware
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+app.use('/users', userController)
+app.use('/birthdays', birthdayController)
 
 // SEQUELIZE CONNECTION
 const sequelize = new Sequelize(process.env.PG_URI)
@@ -17,5 +32,16 @@ try {
 
 
 //Root
+app.get('/', (req, res) => {
+    res.render('landing')
+})
 
-//Listen
+
+app.get ('*', (req, res) => {
+    res.render('error404')
+})
+
+
+const PORT = process.env.PORT
+
+app.listen(PORT, console.log(`listening on port ${PORT}`))
