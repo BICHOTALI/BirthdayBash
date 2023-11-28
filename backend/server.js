bb04edd4ec4236df3ad2f4dc8d46947b405146b7
robertnelson-co <hello@robertnelson.co>
2023-11-27 21:04:23 -0500

    setting up database successful, ensure that pgAdmin is running and that PG_URI and PORT are properly defined in .env file :)

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,20 @@
 //Dependencies
 
+const { Sequelize } = require('sequelize')
+require('dotenv').config();
+
 //Configuration/Middleware
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI)
+  
+try {
+    sequelize.authenticate() 
+    console.log(`Connected with Sequelize at ${process.env.PORT}`) 
+} catch(err) {
+    console.log(`Unable to connect to PG: ${err}`) 
+}
+
 
 //Root
 
