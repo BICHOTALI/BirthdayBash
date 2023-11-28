d59c05737e526171b6f9ca29190eb0f217eb24be
ALISON SALINAS <gisellsalinas43@gmail.com>
2023-11-27 19:34:53 -0500

    syntax pt 2

--- backend/controllers/birthdays.js
+++ backend/controllers/birthdays.js
@@ -29,7 +29,7 @@ router.get('/', async (req, res) => {
 router.get('/:birthdayId', async (req, res) => {
     let birthdayId = Number(req.params.birthdayId)
     if (isNaN(birthdayId)) {
-        res.status(404).json({ message: Invalid id "${birthdayId}" })
+        res.status(404).json({ message: `Invalid id "${birthdayId}" `})
     } else {
         const birthday = await Birthday.findOne({
             where: { birthdayId: birthdayId },
