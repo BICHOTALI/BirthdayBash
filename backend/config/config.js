7f020984a6d501fc4d5b8ddacf82c7d97ef65ce2
robertnelson-co <hello@robertnelson.co>
2023-11-29 21:41:16 -0500

    revised config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 
 module.exports={
   "development": {
-    "username": 'postgres',
-    "password": 'Str0pl3!',
-    "database": 'birthdaybash',
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
