aa0bd3b92ff4a85885d3b0802a318a8cf20532ee
Nick Lafauci <nicholas.lafauci@gmail.com>
2023-11-29 19:47:37 -0500

    birthday forms edited

---
+++ frontend/src/birthdays/EditBirthdayForm.jsx
@@ -0,0 +1,90 @@
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+function EditBirthdayForm({ birthdayId }) {
+  const history = useHistory();
+
+  const [birthday, setBirthday] = useState({
+    name: '',
+    day: '',
+    month: '',
+    year: '',
+  });
+
+  useEffect(() => {
+    // Fetch birthday data based on the provided birthdayId
+    const fetchBirthdayData = async () => {
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVER_URL}birthdays/${birthdayId}`
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        // Set birthday state with fetched data
+        setBirthday(data);
+      } else {
+        // Handle error
+        console.error("Failed to fetch birthday data");
+      }
+    };
+
+    fetchBirthdayData();
+  }, [birthdayId]);
+
+  const handleDateChange = (date) => {
+    // Extract day, month, and year from the selected date
+    const day = date.getDate();
+    const month = date.getMonth() + 1; // Months are 0-indexed
+    const year = date.getFullYear();
+
+    setBirthday({ ...birthday, day, month, year });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await fetch(`${process.env.REACT_APP_SERVER_URL}birthdays/${birthdayId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(birthday),
+    });
+
+    history.push('/places');
+  };
+
+  return (
+    <main>
+      <h1>Edit Birthday</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            required
+            value={birthday.name}
+            onChange={(e) => setBirthday({ ...birthday, name: e.target.value })}
+            className="form-control"
+            id="name"
+            name="name"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="birthdate">Birthdate</label>
+          <DatePicker
+            selected={new Date(birthday.year, birthday.month - 1, birthday.day)}
+            onChange={handleDateChange}
+            className="form-control"
+            id="birthdate"
+            dateFormat="MM/dd/yyyy"
+          />
+        </div>
+        <input className="btn btn-primary" type="submit" value="Update Birthday" />
+      </form>
+    </main>
+  );
+}
+
+export default EditBirthdayForm;
--- frontend/src/birthdays/NewBirthdayForm.jsx
+++ frontend/src/birthdays/NewBirthdayForm.jsx
@@ -1,70 +1,70 @@
-import { useState } from "react"
-import { useHistory } from "react-router"
+import { useState } from "react";
+import { useHistory } from "react-router";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-function NewPlaceForm() {
+function NewBirthdayForm() {
+  const history = useHistory();
 
-	const history = useHistory()
+  const [birthday, setBirthday] = useState({
+    name: '',
+    day: '',
+    month: '',
+    year: '',
+  });
 
-	const [birthday, setBirthday] = useState({
-		name: '',
-		day: '',
-		month: '',
-	})
+  const handleDateChange = (date) => {
+    // Extract day, month, and year from the selected date
+    const day = date.getDate();
+    const month = date.getMonth() + 1; // Months are 0-indexed
+    const year = date.getFullYear();
 
-	async function handleSubmit(e) {
-		e.preventDefault()
+    setBirthday({ ...birthday, day, month, year });
+  };
 
-		await fetch(process.env.REACT_APP_SERVER_URL + `birthdays`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(birthday)
-		})
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-		history.push('/places')
-	}
+    await fetch(process.env.REACT_APP_SERVER_URL + `birthdays`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(birthday),
+    });
 
-	return (
-		<main>
-			<h1>Add Your Birthday!</h1>
-			<form onSubmit={handleSubmit}>
-				<div className="form-group">
-					<label htmlFor="name">Name</label>
-					<input
-						required
-						value={place.name}
-						onChange={e => setPlace({ ...place, name: e.target.value })}
-						className="form-control"
-						id="name"
-						name="name"
-					/>
-				</div>
-				<div className="Month">
-					<label htmlFor="founded">Birth Month</label>
-					<input
-						required
-						value={place.founded}
-						onChange={e => setPlace({ ...place, founded: e.target.value })}
-						className="form-control"
-						id="month"
-						name="month"
-					/>
-				</div>
-				<div className="form-group">
-					<label htmlFor="Month">Day</label>
-					<input
-						value={place.city}
-						onChange={e => setPlace({ ...place, city: e.target.value })}
-						className="form-control"
-						id="city"
-						name="city"
-					/>
-				</div>
-				<input className="btn btn-primary" type="submit" value="Add Place" />
-			</form>
-		</main>
-	)
+    history.push('/places');
+  };
+
+  return (
+    <main>
+      <h1>Add Your Birthday!</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            required
+            value={birthday.name}
+            onChange={(e) => setBirthday({ ...birthday, name: e.target.value })}
+            className="form-control"
+            id="name"
+            name="name"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="birthdate">Birthdate</label>
+          <DatePicker
+            selected={new Date(birthday.year, birthday.month - 1, birthday.day)}
+            onChange={handleDateChange}
+            className="form-control"
+            id="birthdate"
+            dateFormat="MM/dd/yyyy"
+          />
+        </div>
+        <input className="btn btn-primary" type="submit" value="Add Place" />
+      </form>
+    </main>
+  );
 }
 
-export default NewPlaceForm
+export default NewBirthdayForm;
